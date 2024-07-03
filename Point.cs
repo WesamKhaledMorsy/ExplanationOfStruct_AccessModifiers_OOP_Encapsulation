22717771392b996fda67f7c21f4ff5533f20cfa2
Wesam Khaled Morsy <wesamkhaledmorsy@gmail.com>
2024-07-04 01:35:23 +0200

    Add project files.

---
+++ Point.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo06
+{
+    //public or internal
+    struct Point
+    {
+        ///1. Attributes
+        ///
+
+        public double x;
+        public double y;
+
+        /// Functions 
+        /// special function [Constructors] 
+        /// 1. named always with the same name of struct name or the class name
+        /// benefits of struct:
+        /// 1.has no return
+        /// 2.initialize for attributes
+        ///
+
+        /// Allowed Access modifiers >> public / internel
+        /// we can use >> Private >> only if we will use [singlton Dp] >>> this class we ant to create one object only from it
+        /// takes parameters or not
+        /// 
+
+        public Point()
+        {
+            x=100;
+            y=default;
+        }
+
+        //initialize for attributes
+        public Point(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+        }
+        public Point(double _x, int _y)
+        {
+            x = _x;
+            y = _y;
+        }
+
+        // OR
+        //public Point(int x, int y)  // in .net6.00
+        //{
+        //    this.x = x;
+        //    this.y = y;
+        //}
+
+        // Constructor overloading == >>  عددهم و ترتيب و نوع المتغيرات
+        public Point(int num)
+        {
+            x = y = num;
+        }
+
+        //public Point(int _x, string _y) // invalid
+        //{
+        //    x = _x;
+        //    y = _y;
+        //}
+        //public Point(string _x, int _y)  // invalid
+        //{
+        //    x = _x;
+        //    y = _y;
+        //}
+
+      
+      
+        // overloading of ToString() method to retrieve the actual data
+        public override string ToString() {
+            return $"X = {x} , Y ={y}";
+        }
+
+        // that is tha default of tostring();
+        //public override string ToString()
+        //{
+        //    return base.ToString();
+        //}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using Common;
+using System.Drawing;
+namespace Demo06
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Pros & cons >> according to memory Allocation
+        /// pros: 1. quick to access its attributes as it in stack 
+        ///       2. 
+        /// cons: 1. if the object I will use it alot so I couldn't use it
+        ///       2. his lifetime ends at the function end
+        /// </summary>
+       
+
+        static void Main(string[] args)
+        {
+            // that if TypeA is public
+            TypeA typeA = new TypeA();
+            //typeA.y = 1; //is inaccessible due to its protection level as internal and it called in another project
+            typeA.z =5;
+
+            ////////////////////////
+
+            Point p1;
+            //Console.WriteLine(p1.y); // invalid before assign value to it >> as it is in garbage
+            
+            // by default the compilar will generate paramter less constructor 
+            p1 = new Point(); // but new here is not the same with the new of class
+            /// new class() ==>> 1. allocate required bytes in heap
+            /// 2. intialize these bytes with the default value of it
+            /// 3. call the constructor
+            /// 4. make the reference refer to this object
+            /// but in struct ==>> 1. which constructor to use to initialize (x ,y),,( Constructor selection) 
+            
+
+            Console.WriteLine(p1.x+ " "+ p1.y); // valid here 
+            // Declare of object of type Point
+            // CLR allocate 8 bytes uninitialized bytes at stack
+
+            Point p2= new Point(100 ,5);
+            Console.WriteLine(p1.ToString()); //Demo06.Point >> so we will override this method to return a value
+            // after override tostring 
+            Console.WriteLine(p1); // X = 100 , Y =0
+            Console.WriteLine(p1.ToString()); // X = 100 , Y =0
+            int number = 10;
+            Console.WriteLine(number.ToString()); //10
+
+        }
+    }
+}
